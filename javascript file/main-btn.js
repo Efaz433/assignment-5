1940bd2ba238c45585fe1b81b681fb65e164108f
sayeed ahmed efaz <efazsayeedahmed@gmail.com>
2025-03-04 00:58:44 +0600

    partialy add activity section

--- javascript file/main-btn.js
+++ javascript file/main-btn.js
@@ -27,30 +27,30 @@
 
 document.getElementById("main-btn-1")
     .addEventListener("click",function(){
-         alertFunction("main-btn-1")
+         alertFunction("main-btn-1", document.getElementById("card-1-title").innerText)
 });
 document.getElementById("main-btn-2")
     .addEventListener("click",function(){
-         alertFunction("main-btn-2")
+         alertFunction("main-btn-2", document.getElementById("card-2-title").innerText)
 });
 document.getElementById("main-btn-3")
     .addEventListener("click",function(){
-         alertFunction("main-btn-3")
+         alertFunction("main-btn-3", document.getElementById("card-3-title").innerText)
 });
 document.getElementById("main-btn-4")
     .addEventListener("click",function(){
-         alertFunction("main-btn-4")
+         alertFunction("main-btn-4", document.getElementById("card-4-title").innerText)
 });
 document.getElementById("main-btn-5")
     .addEventListener("click",function(){
-         alertFunction("main-btn-5")
+         alertFunction("main-btn-5", document.getElementById("card-5-title").innerText)
 });
 document.getElementById("main-btn-6")
     .addEventListener("click",function(){
-         alertFunction("main-btn-6")
+         alertFunction("main-btn-6", document.getElementById("card-6-title").innerText)
 });
 
-function alertFunction(id){
+function alertFunction(id,taskName){
     // to give alert message
     alert("Board Updated successfully")
     document.getElementById(id).disabled = true;
@@ -65,4 +65,11 @@ function alertFunction(id){
     const convertedIncrease = parseInt(willIncrease);
     const increment = convertedIncrease + 1 ;
     document.getElementById("will-increase").innerText = increment ;
+
+    // for history
+    const historyContainer = document.getElementById("history-container");
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <h2 class = "bg-slate-100">you have completed ${taskName} task </h2>`
+    historyContainer.appendChild(div);
 }
