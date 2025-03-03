ede48975523a79d6be36b6b9fe6b2596749d769a
sayeed ahmed efaz <efazsayeedahmed@gmail.com>
2025-03-03 13:16:24 +0600

    added header

---
+++ javascript file/color-changing-btn.js
@@ -0,0 +1,10 @@
+let colors = ["slateblue", "lightblue", "skyblue", "deepskyblue", "dodgerblue", "royalblue", "blue", "mediumblue", "darkblue", "navy", "midnightblue"];  
+let i = 0;
+document.getElementById('color-changing-btn')
+    .addEventListener('click', function(){
+        document.body.style.backgroundColor = colors[i];
+        i++;
+        if(i > colors.length){
+            i = 0;
+        }
+})
