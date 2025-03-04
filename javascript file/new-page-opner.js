3e7f9f0823a9c8ada66a6ba7d12f4ba12359a38d
sayeed ahmed efaz <efazsayeedahmed@gmail.com>
2025-03-04 23:53:33 +0600

    added everything '

--- javascript file/new-page-opner.js
+++ javascript file/new-page-opner.js
@@ -1,5 +1,5 @@
 document.getElementById("new-page-inside")
     .addEventListener("click", function(){
-        window.location.href = "../new-page.html";
+        window.location.href = "../index-2.html";
     })
 
