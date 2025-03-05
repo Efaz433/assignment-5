0e1bd27e1a3010475fffa40b80ef82bd8292fe2b
sayeed ahmed efaz <efazsayeedahmed@gmail.com>
2025-03-05 16:03:12 +0600

    pixed problem problem

--- javascript file/previous-page.js
+++ javascript file/previous-page.js
@@ -1,3 +1,3 @@
 document.getElementById("pervious-page").addEventListener("click", function(){
-    window.location.href = "../index.html";
+    window.location.href = "../assignment-5/index.html";
 })
