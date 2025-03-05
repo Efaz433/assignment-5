c920f8062e65b8ed55085224d1ccf2b27cde7c3d
sayeed ahmed efaz <efazsayeedahmed@gmail.com>
2025-03-05 15:58:36 +0600

    add everything

--- javascript file/new-page-opner.js
+++ javascript file/new-page-opner.js
@@ -1,5 +1,5 @@
 document.getElementById("new-page-inside")
     .addEventListener("click", function(){
-        window.location.href = "../index-2.html";
+        window.location.href = "../assignment-5/index-2.html";
     })
 
